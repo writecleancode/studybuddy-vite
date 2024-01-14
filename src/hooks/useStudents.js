e8742eb7a136df8af16249774b53e4f44d4f25b5
Rafal <rafek131@wp.pl>
2024-01-14 22:19:25 +0100

    added axios interceptors with authorization token

--- src/hooks/useStudents.js
+++ src/hooks/useStudents.js
@@ -1,6 +1,21 @@
 import { useCallback } from 'react';
 import axios from 'axios';
 
+const studentsAPI = axios.create({});
+
+studentsAPI.interceptors.request.use(
+	config => {
+		const token = localStorage.getItem('token');
+
+		if (token) config.headers.authorization = `Bearer ${token}`;
+
+		return config;
+	},
+	function (error) {
+		return Promise.reject(error);
+	}
+);
+
 export const useStudents = () => {
 	const getGroups = useCallback(async () => {
 		try {
